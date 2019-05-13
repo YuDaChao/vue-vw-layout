506f53c6294e9c31f35aeb4a3b488c884edfd41d
happyu <290945266@qq.com>
2019-05-13 23:40:07 +0800

    feat: init

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-aspect-ratio-mini': {},
+    'postcss-write-svg': {
+      utf8: false
+    },
+    'postcss-cssnext': {},
+    'postcss-px-to-viewport': {
+      viewportWidth: 640, // 设备的宽度 对应的事设计稿的宽度
+      viewportHeight: 1334, // 设备的高度 根据750设备的宽度来指定 一般指定1334 也可以不指定
+      unitPrecision: 3, // 指定`px`转换为视窗单位的小数位数
+      viewportUnit: 'vw', // 指定需要转换的视窗单位 建议使用vw
+      selectorBlackList: ['.ignore', '.hairlines'], // 指定不转换为视窗单位的类 可以自定义
+      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位
+      mediaQuery: false // 允许在媒体查询中转换`px`
+    },
+    'cssnano': {
+      preset: 'advanced',
+      autoprefixer: false,
+      'postcss-zindex': false
+    }
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box">在Vue中使用vw</div>
+    <div class="ignore">此处不需要转换为视窗单位</div>
+    <div aspectratio w-100-100>
+      <div aspectratio-content>
+        此处主要用来处理元素容器宽高比
+      </div>
+    </div>
+    <div class="example">处理1px</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.box {
+  border: 1px solid black;
+  font-size: 20px;
+  line-height: 28px;
+  padding: 10px;
+  margin-bottom: 24px;
 }
+.ignore {
+  font-size: 24px;
+  border: 2px solid pink;
+  padding: 10px;
+  margin-bottom: 24px;
+}
+[aspectratio] {
+  position: relative;
+}
+[aspectratio]::before {
+  content: '';
+  display: block;
+  height: 0;
+}
+[aspectratio-content] {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
+[w-100-100] {
+  aspect-ratio: '100:100';
+}
+[w-100-100] {
+  background: pink;
+  color: #fff;
+  margin-bottom: 24px;
+}
+
+@svg 1px-border {
+  height: 2px;
+  @rect {
+    fill: var(--color, black);
+    width: 100%;
+    height: 50%;
+  }
+}
+
+.example {
+  border-bottom: 1px solid transparent;
+  border-image: svg(1px-border param(--color #00b1ff)) 2 2 stretch;
+}
+
+// @svg square {
+//   @rect {
+//     fill: var(--color, black);
+//     width: 100%; height: 1px;
+//   }
+// };
+
+// .example {
+//   background: white svg(square param(--color #00b1ff))  center / cover;
+// }
+
 </style>
